741d0e536edf417f4897472f92ba16fd1e3871ff
Vladimirsky <vladimir.bervin@gmail.com>
2021-07-20 23:53:59 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiKey: 'AIzaSyA66mcq3hJeN_IrkWryCCybATUVUBIJNJE',
+  apiKey: '',
   fbDbUrl: 'https://online-shop-5096f-default-rtdb.europe-west1.firebasedatabase.app/'
 };
 
